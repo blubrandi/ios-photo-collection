939cf2c26b125758ba020c1e2e5677bd5a1373bf
Brandi <blubrandi0@gmail.com>
2019-10-17 12:39:38 -0600

    Created PhotoModel

---
+++ PhotoCollection/PhotoCollection/Models/PhotoModel.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoModel.swift
+//  PhotoCollection
+//
+//  Created by Brandi on 10/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
